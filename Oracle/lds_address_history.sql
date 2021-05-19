538339e5a48e35bc1bff3a5e5535392250d7ded8
Lav Patel <contact.lavpatel@gmail.com>
2021-05-19 15:00:49 -0500

    working lds_address_history table on nheron

---
+++ Oracle/lds_address_history.sql
@@ -0,0 +1,98 @@
+alter session set current_schema = pcornet_cdm;
+/** immunization - create and populate the immunization table.
+*/
+insert into cdm_status (task, start_time) select 'lds_address_history', sysdate from dual
+/
+
+
+BEGIN
+PMN_DROPSQL('DROP TABLE lds_address_history');
+END;
+/
+
+CREATE TABLE lds_address_history ( 
+    "ADDRESSID" VARCHAR2(20 BYTE) NOT NULL ENABLE, 
+	"PATID" VARCHAR2(50 BYTE) NOT NULL ENABLE, 
+	"ADDRESS_USE" VARCHAR2(2 BYTE) NOT NULL ENABLE, 
+	"ADDRESS_TYPE" VARCHAR2(2 BYTE) NOT NULL ENABLE, 
+	"ADDRESS_PREFERRED" VARCHAR2(2 BYTE) NOT NULL ENABLE, 
+	"ADDRESS_CITY" VARCHAR2(50 BYTE), -- there some null city
+	"ADDRESS_STATE" VARCHAR2(2 BYTE), -- there some null state
+	"ADDRESS_ZIP5" VARCHAR2(5 BYTE) , 
+	"ADDRESS_ZIP9" VARCHAR2(9 BYTE), 
+	"ADDRESS_PERIOD_START" DATE,
+    "ADDRESS_PERIOD_END" DATE
+   );
+/QL('DROP sequence  lds_address_history_seq');
+END;
+/
+
+create sequence  lds_address_history_seq
+/
+
+create or replace trigger lds_address_history_trg
+before insert on lds_address_history
+for each row
+begin
+  select lds_address_history_seq.nextval into :new.ADDRESSID from dual;
+end;
+/
+
+insert /*+ APPEND */ into lds_address_history(
+     PATID
+	,ADDRESS_USE
+	,ADDRESS_TYPE
+	,ADDRESS_PREFERRED
+	,ADDRESS_CITY
+	,ADDRESS_STATE
+	,ADDRESS_ZIP5
+	,ADDRESS_ZIP9
+	,ADDRESS_PERIOD_START
+    ,ADDRESS_PERIOD_END
+)
+select 
+     patient_num patid
+    , 'HO' ADDRESS_USE --hardcoding and assuming all addresses will be home address 
+    , case 
+        when 
+            upper(add_line_1) like '%BOX%' 
+            and
+                (
+                upper(add_line_1) like '%PO%' 
+                or upper(add_line_1) like '%P O%'
+                or upper(add_line_1) like '%P.O.%'
+                )
+        then 'PO'
+        ELSE 'PH'
+    END  ADDRESS_TYPE
+    , 'Y' ADDRESS_PREFERRED --we are only storing one address per patients, so assuming that is prefered address
+    , city ADDRESS_CITY
+    , scode.code ADDRESS_STATE
+    , CASE 
+            WHEN length(zip_cd)=5 
+                THEN zip_cd
+            WHEN length(zip_cd)>5 
+                THEN substr(zip_cd,1,5)
+            else null
+      END ADDRESS_ZIP5
+    , CASE 
+            WHEN length(zip_cd)=10 or length(zip_cd)=10 -- some time there is dash in zip, so length can be 9 or 10
+                THEN replace(zip_cd,'-','')
+            else null
+      END ADDRESS_ZIP9
+    , download_date ADDRESS_PERIOD_START 
+    , null ADDRESS_PERIOD_START
+        
+from NIGHTHERONDATA.patient_dimension pdim
+left join pcornet_cdm.state_code scode 
+    on upper(scode.state) = upper(pdim.state_cd)
+/
+
+commit
+/
+
+update cdm_status
+set end_time = sysdate, records = (select count(*) from lds_address_history)
+where task = 'lds_address_history'
+;
+/
